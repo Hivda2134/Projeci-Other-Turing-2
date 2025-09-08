75569b27f78cda15c1e94ef4c7a1040b2355a463
Hivda Karakas <hivdakarakas38@gmail.com>
2025-09-08 14:04:31 +0300

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="lmutual",
-    version="0.2.0",
-    packages=find_packages(),
-    install_requires=["pytest>=7", "jsonschema>=4"],
-    python_requires=">=3.9",
-)
